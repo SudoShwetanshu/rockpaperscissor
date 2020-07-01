edf6a9cdb29c2857a6ea42f8c33f1f1a59543335
SudoShwetanshu <67130226+SudoShwetanshu@users.noreply.github.com>
2020-07-01 13:42:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+A classic game of RPS made in association with The Odin Project. It can be be played at [GitHub Pages](https://emilravn.github.io/rockpaperscissors/).
+
+## Rules of Rock Paper Scissors
+
+Each turn consists of the player making a distinct choice and the computer making a random choice of either "rock", "paper" or "scissors". The first to reach 5 points win the game. A draw does not equal to a point.
+
+- Rock beats Scissors.
+- Paper beats Rock.
+- Scissors beats Paper.
+
+Have fun!
---
+++ script.js
@@ -0,0 +1,112 @@
+// Variables for selection
+let choice = ["Rock", "Paper", "Scissors"];
+let computerScore = 0;
+let playerScore = 0;
+
+// Buttons for the player options
+const rockBtn = document.querySelector("#rockbtn");
+const paperBtn = document.querySelector("#paperbtn");
+const scissorsBtn = document.querySelector("#scissorsbtn");
+
+// Reset button
+const resetBtn = document.createElement("button");
+resetBtn.innerHTML = "Reset";
+resetBtn.setAttribute("style", "background-color: red; width:120px; height: 60px; font-size: 14px; color: white; border-style: hidden; border-radius: 6px; text-decoration: none; overflow: hidden; cursor: pointer; text-align: center; -transition-duration: 0.4s; outline: 0; margin: 40px 0px; resetBtn:hover");
+
+// Rock Pick
+rockBtn.addEventListener("click", () => {
+  playerChoice = choice[0];
+  Versus();
+});
+
+// Paper Pick
+paperBtn.addEventListener("click", () => {
+  playerChoice = choice[1];
+  Versus();
+});
+
+// Scissors Pick
+scissorsBtn.addEventListener("click", () => {
+  playerChoice = choice[2];
+  Versus();
+});
+
+const updateScore = () => {
+  if (playerScore === 5 || computerScore === 5) {
+    if (playerScore > computerScore) {
+      document.getElementById(
+        "scoreboard"
+      ).textContent = `You win the game! ${playerChoice} beats ${computerChoice}.`;
+      RemoveOptions();
+    } else {
+      document.getElementById(
+        "scoreboard"
+      ).textContent = `The computer wins the game! ${playerChoice} is beaten by ${computerChoice}.`;
+      RemoveOptions();
+    }
+  }
+  document.getElementById("ps").textContent = playerScore;
+  document.getElementById("cs").textContent = computerScore;
+};
+
+function ComputerPick() {
+  return choice[Math.floor(Math.random() * 3)];
+}
+
+function lossAlert() {
+  document.getElementById(
+    "scoreboard"
+  ).textContent = `You lose! ${playerChoice} is beaten by ${computerChoice}.`;
+  computerScore++;
+}
+
+function Versus() {
+  PlayRound(playerChoice);
+  updateScore();
+}
+
+function PlayRound(playerChoice) {
+  window.computerChoice = ComputerPick();
+  if (computerChoice === choice[0] && playerChoice === choice[2]) {
+    lossAlert();
+  } else if (computerChoice === choice[1] && playerChoice === choice[0]) {
+    lossAlert();
+  } else if (computerChoice === choice[2] && playerChoice === choice[1]) {
+    lossAlert();
+  } else if (computerChoice === playerChoice) {
+    document.getElementById(
+      "scoreboard"
+    ).textContent = `That's a draw! Computer picked ${computerChoice} and you picked ${playerChoice}.`;
+  } else {
+    document.getElementById(
+      "scoreboard"
+    ).textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
+    playerScore++;
+  }
+}
+
+const resetGame = document.querySelector("resetContainer");
+
+resetBtn.addEventListener("click", () => {
+  document.getElementById("ps").innerHTML = "0";
+  document.getElementById("cs").innerHTML = "0";
+  document.getElementById("scoreboard").innerHTML =
+    "New game! Choose your pick!";
+  playerScore = 0;
+  computerScore = 0;
+  AddOptions();
+});
+
+function RemoveOptions() {
+  document.getElementById("input").removeChild(rockBtn);
+  document.getElementById("input").removeChild(paperBtn);
+  document.getElementById("input").removeChild(scissorsBtn);
+  document.getElementById("resetContainer").appendChild(resetBtn);
+}
+
+function AddOptions() {
+  document.getElementById("input").appendChild(rockBtn);
+  document.getElementById("input").appendChild(paperBtn);
+  document.getElementById("input").appendChild(scissorsBtn);
+  document.getElementById("resetContainer").removeChild(resetBtn);
+}
